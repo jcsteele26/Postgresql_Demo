72d1df43d59e88f48d4d8fff02cc0975437156ab
Andre <jcsteele26@gmail.com>
2020-08-16 18:21:34 -0400

    Start code for demo

---
+++ postgresqldemo/src/main/java/com/steele/postgresqldemo/Song.java
@@ -0,0 +1,124 @@
+package com.steele.postgresqldemo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Song {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String artist;
+    private String lyrics;
+    private String genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Song(String title, String artist, String lyrics, String genre) {
+        this.title = title;
+        this.artist = artist;
+        this.lyrics = lyrics;
+        this.genre = genre;
+    }
+
+    public Song() {
+    }
+
+    @Override
+    public String toString() {
+        return "Song [artist=" + artist + ", genre=" + genre + ", id=" + id + ", lyrics=" + lyrics + ", title=" + title
+                + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+        result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((lyrics == null) ? 0 : lyrics.hashCode());
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Song other = (Song) obj;
+        if (artist == null) {
+            if (other.artist != null)
+                return false;
+        } else if (!artist.equals(other.artist))
+            return false;
+        if (genre == null) {
+            if (other.genre != null)
+                return false;
+        } else if (!genre.equals(other.genre))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (lyrics == null) {
+            if (other.lyrics != null)
+                return false;
+        } else if (!lyrics.equals(other.lyrics))
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        return true;
+    }
+   
+    
+}
