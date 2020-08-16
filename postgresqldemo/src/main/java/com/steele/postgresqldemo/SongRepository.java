db57bf7e5b2b451d588b392f9bc69c9c34421e76
Andre <jcsteele26@gmail.com>
2020-08-16 18:34:56 -0400

    Code for repository

---
+++ postgresqldemo/src/main/java/com/steele/postgresqldemo/SongRepository.java
@@ -0,0 +1,9 @@
+package com.steele.postgresqldemo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SongRepository extends JpaRepository<Song, Long> {
+
+    Song findByName(String title);
+    
+} 
